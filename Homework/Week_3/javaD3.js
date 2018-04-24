415a4833c7e63a9175d880d8891997df24e7ee37
Oranguh <makoheuvelman@hotmail.com>
2018-04-24 23:40:00 +0200

    stuffies bar chart first attempt

--- Homework/Week_3/javaD3.js
+++ Homework/Week_3/javaD3.js
@@ -1,27 +1,33 @@
 function reqListener(){
 
-  var jsondata = this.responseText;
-  console.log(jsondata)
-  // var json_values = jsondata.map(x => x["fraud_count"]);
-  console.log(json_values)
+  var jsondata = JSON.parse(this.responseText);
+  var json_values = []
+  var json_percentage = []
+  var json_months = []
+
+  for (i in jsondata) {
+    json_values[i] = jsondata[i]["fraud_count"]
+    json_months[i] = jsondata[i]["month"]
+  }
+  console.log(json_percentage)
   var svg = d3.select("div.output svg")
     .attr('width', window.innerWidth)
     .attr('height', window.innerHeight)
   // console.log(svg)
   svg.selectAll("rect")
-    .data([12, 6, 25])
+    .data(json_values)
     .enter().append("rect")
+      .attr("class", "bar")
       .attr("x", 0)
-      .attr("y", function(d,i) { return i*30+50 })
-      .attr("width", function(d,i) { return d; })
-      .attr("height", 20)
+      .attr("y", function(d,i) { return i*15 })
+      .attr("width", function(d,i) { return d*2; })
+      .attr("height", 15)
       .style("fill", "steelblue")
-
 }
 
 var requester = new XMLHttpRequest();
-var gitthingy = "https://oranguh.github.io/dataprocessing/Homework/Week_3/enron.json";
-
+// var gitthingy = "https://oranguh.github.io/dataprocessing/Homework/Week_3/enron.json";
+var gitthingy = "enron.json"
 requester.addEventListener("load", reqListener);
 requester.open("GET", gitthingy);
 requester.send();
