5296c488bd94305a40707ea310daa24cb944fb74
Oranguh <makoheuvelman@hotmail.com>
2018-04-26 01:34:03 +0200

    finally made the axis, now need to add labels and some tooltips

--- Homework/Week_3/javaD3.js
+++ Homework/Week_3/javaD3.js
@@ -1,101 +1,112 @@
 function reqListener(){
 
   var jsondata = JSON.parse(this.responseText);
-  var json_values = [];
+  // var json_values = [];
   var json_percentage = [];
   var json_months = [];
 
   // console.log(typeof this.responseText)
   // console.log(typeof jsondata)
 
-
+var maxfinder = []
   for (i in jsondata.DATA) {
-    json_values[i] = Number(jsondata.DATA[i]["fraud_count"]);
-    json_months[i] = new Date(jsondata.DATA[i]["month"]*1000).toString();
+    // json_values[i] = Number(jsondata.DATA[i]["fraud_count"]);
+    jsondata.DATA[i]["month"] = new Date(jsondata.DATA[i]["month"]*1000).toString();
+    maxfinder[i] = jsondata.DATA[i].fraud_count
   }
   // console.log(json_values)
   var w = window.innerWidth - 100;
   var h = window.innerHeight - 100;
-  var margins = {"right": 5, "left":50, "bottom": 20, "top": 20};
+  var margins = {"right": 0, "left":40, "bottom": 30, "top": 0};
   var barPadding = 3;
 
+  // console.log(Math.max(...json_values))
 
   var xs = d3.scale.ordinal()
-      .rangeRoundBands([0, w], .1);
+      .rangeRoundBands([margins.left, w]);
 
+// d3.max(jsondata.DATA, function(d) { return d.fraud_count;})
   var ys = d3.scale.linear()
-      .range([h, 0]);
+      .domain([Math.max(...maxfinder), 0])
+      .range([0, (h - margins.bottom)]);
+
+      // console.log(Math.max(...maxfinder))
+      // console.log(ys(10))
+      // console.log((h - margins.bottom))
 
   var xAxis = d3.svg.axis()
       .scale(xs)
+      // .ticks(jsondata.DATA.length)
       .orient("bottom");
 
   var yAxis = d3.svg.axis()
       .scale(ys)
       .orient("left")
-      .tickFormat(d3.format(".0%"));
+      // .tickFormat(d3.format(".0%"));
+      .ticks(5);
 
   var svg = d3.select("body")
               .append("svg")
               .attr("width", w)
-              .attr("height", h);
+              .attr("height", h)
 
-  // var g = svg.append("g")
-  //     .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
 
   svg.selectAll("rect")
-    .data(json_values)
+    .data(jsondata.DATA)
     .enter()
     .append("rect")
     .attr("class", "bar")
     .attr("x", function(d, i) {
-    return i * (w / json_values.length) + margins.left;
+    return Math.floor(i * (w / jsondata.DATA.length) + margins.left)
     })
     .attr("y", function(d) {
-    return (h - margins.bottom) - d;  //Height minus data value
+    return (h - (h -ys(d.fraud_count)));  //Height minus data value
     })
-    .attr("width", w / json_values.length - barPadding)
+
+    .attr("width", Math.floor(w / jsondata.DATA.length - barPadding))
     .attr("height", function(d) {
-    return d;  //Just the data value
+    return h - ys(d.fraud_count) - margins.bottom;  //Just the data value
   });
-    // .attr("fill", function(d) {
-    // return "rgb(0, " + (d * 0.5) + ", 0)";
-    // });
-
-
-  // svg.append("g")
-  //     .attr("class", "x axis")
-  //     .attr("transform", "translate(0," + h + ")")
-  //     .call(xAxis);
-  // svg.append("g")
-  //     .attr("class", "y axis")
-  //     .call(yAxis)
-  //   .append("text")
-  //     .attr("transform", "rotate(-90)")
-  //     .attr("y", 6)
-  //     .attr("dy", ".71em")
-  //     .style("text-anchor", "end")
-  //     .text("Frequency");
 
   svg.selectAll("text")
-   .data(json_months)
+   .data(jsondata.DATA)
    .enter()
    .append("text")
    .attr("class", "texto")
    .text(function(d) {
-     return d.substring(3,7)
+     return d.month.substring(3,7)
    })
    .attr("x", function(d, i) {
-        return i * (w / json_values.length) + margins.left + 4;
+        return Math.floor(i * (w / jsondata.DATA.length) + margins.left) ;
    })
    .attr("y", function(d) {
-        return h - 5;
+        return h - margins.bottom * 0.4;
    });
+   svg.selectAll("text1")
+    .data(jsondata.DATA)
+    .enter()
+    .append("text")
+    .attr("class", "texto dato")
+    .text(function(d) {
+      return d.month.substring(10,15)
+    })
+    .attr("x", function(d, i) {
+         return Math.floor(i * (w / jsondata.DATA.length) + margins.left) ;
+    })
+    .attr("y", function(d) {
+         return h - 3 ;
+    });
+  svg.append('g')
+    .attr("transform", "translate(0," + (h - margins.bottom) + ")")
+    .call(xAxis)
+  svg.append('g')
+    .attr("transform", "translate(" + margins.left + ", 0)")
+    .call(yAxis)
 
   //  I used this helpful link to make this http://jsfiddle.net/nhHww/1/
   svg.append("text")
     .attr("class", "texturl")
-    .attr("x", "20")
+    .attr("x", "50")
     .attr("y", "20")
     .attr("font-family", "sans-serif")
     .attr("font-size", "20px")
@@ -104,7 +115,7 @@ function reqListener(){
     .on("click", function() { window.open("https://www.cs.cmu.edu/~enron/");
     });
     svg.append("text")
-      .attr("x", "20")
+      .attr("x", "50")
       .attr("y", "50")
       .attr("font-family", "sans-serif")
       .attr("font-size", "15px")
