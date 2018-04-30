5a525b145c7199a537343c001302a11d08912c53
oranguh <makoheuvelman@hotmail.com>
2018-05-01 00:02:30 +0200

    stuff

---
+++ Homework/Week_4/yelpo_scatter.js
@@ -0,0 +1,20 @@
+// Marco Heuvelman  10176306
+// YELP API KEY:
+// 8ZtylY3CFd_Rnym2ndkiG7Yx8NTJy9d9Gl6sIRSSZqvC00Ep1muPovd6M6S1xnydJK3LwZwfgKHKRRlGqWbiAYLjuzW0rBcPCFPjedt3WHMUr3nKLERWxUhgjYrnWnYx
+
+
+
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", 500)
+            .attr("height", 200)
+console.log(svg)
+svg.selectAll("rect")
+  .enter()
+  .append("rect")
+  .attr("class", "bar") // look at stylers.css for additional attributes
+  .attr("x", 50)
+  .attr("y", 50)
+
+  .attr("width", 6)
+  .attr("height", 7)
