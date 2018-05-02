df201c39e18ee5b84247187b75e4d02d20109483
oranguh <makoheuvelman@hotmail.com>
2018-05-02 11:14:24 +0200

    pokemans coming along

--- Homework/Week_4/yelpo.js
+++ Homework/Week_4/yelpo.js
@@ -7,7 +7,7 @@
 // dropdown for different type filtering
 
 var w = window.innerWidth - 150;
-var h = window.innerHeight - 100;
+var h = window.innerHeight - 50;
 var margins = {"right": 20, "left":60, "bottom": 50, "top": 50};
 var barPadding = 3;
 var numPokemans = 5;
@@ -26,7 +26,7 @@ window.onload = function(){
       if (error) throw error;
 
       pokemans = JSON.parse(pokedex[0]['response']).results
-      console.log(pokemans);
+      // console.log(pokemans);
 
       let helloThere = d3.queue()
       for (let i = 0; i < numPokemans; ++i) {
@@ -45,29 +45,75 @@ window.onload = function(){
       console.log(plokemon);
 
       myPokemonList.push(plokemon)
-      console.log(myPokemonList[0].weight)
-      console.log(myPokemonList[0].stats[0].base_stat)
+      console.log(myPokemonList[i].weight)
+      console.log(myPokemonList[i].stats[0].base_stat)
+      for (let k = 0; k < myPokemonList[i].types.length; k++){
+        console.log(myPokemonList[i].types[k].type.name)
+
+      }
     }
   // AXES and transforms!
+  var xs = d3.scaleLinear()
+      .domain([0, d3.max(myPokemonList, function(d){
+            return d.weight;
+        })])
+      .range([margins.left, (w - margins.right)]);
+
+  var ys = d3.scaleLinear()
+      .domain([d3.max(myPokemonList, function(d){
+            return d.stats[0].base_stat;
+        }), 0])
+      .range([margins.top, (h - margins.bottom)]);
+
+
+
 
   // DO SVG SCATTERPLOT HERE
   var svg = d3.select("body")
     .append("svg")
     .attr("width", w)
     .attr("height", h)
+  // make them axes
+
+  // var axisX = svg.select("#x_axis")
+  //     .transition().duration(200)//.ease(“sin-in-out”)
+  //     .attr("transform", "translate(0," + h + ")")
+  //     .call(d3.axisBottom(xs))
+
+  var xAxis = d3.axisBottom(xs);
+  var yAxis = d3.axisLeft(ys);
+
 
   svg.selectAll("circle")
    .data(myPokemonList)
    .enter()
    .append("circle")
    .attr("cx", function(d) {
-         return d.weight;
+         return Math.floor(xs(d.weight));
     })
     .attr("cy", function(d) {
-         return d.stats[0].base_stat;
+         return Math.floor((h - (h - ys(d.stats[0].base_stat))));
     })
     .attr("r", function(d) {
          return d.height;
+    })
+    .attr("class", function(d) {
+      let pokeclasses = ""
+      for (let k = 0; k < d.types.length; k++){
+        console.log(d.types[k].type.name)
+        pokeclasses = pokeclasses + (d.types[k].type.name) + " "
+      }
+      return pokeclasses
     });
+
+  svg.append('g')
+    .attr("transform", "translate(0," + (h - margins.bottom) + ")")
+    .call(xAxis)
+  svg.append('g')
+    .attr("transform", "translate(" + margins.left + ", 0)")
+    .call(yAxis)
+
+
+
   };
 };
