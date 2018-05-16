fb65f1a24f7ba8ca826a7ccfe209d814ddbf1b8c
oranguh <makoheuvelman@hotmail.com>
2018-05-16 17:18:40 +0200

    making things pretty

--- Homework/Week_5/pokeTree.js
+++ Homework/Week_5/pokeTree.js
@@ -11,56 +11,42 @@
 // https://www.youtube.com/watch?v=iZ6MSHA4FMU&index=14&list=PL6il2r9i3BqH9PmbOf5wA5E1wOG3FT22p
 // http://d3indepth.com/layouts/
 
-// var POKEDEX = 'https://pokeapi.co/api/v2/pokemon/';
-var root
 
+var root
 function getEvolution(pokeURL){
 
-  // console.log("It works")
-  // console.log(pokeURL.species.url)
-
   var svgTree = d3.select("body").select(".treeDiagram")
-
   var pokeTree
   let treeQueue = d3.queue();
   treeQueue.defer(d3.request, pokeURL.species.url)
   treeQueue.await(function(error, pokeSpecies) {
     if (error) throw error;
     pokeSpeci = JSON.parse(pokeSpecies.response)
-    // console.log(pokeSpeci.evolution_chain.url)
     let treeQueue = d3.queue();
     treeQueue.defer(d3.request, pokeSpeci.evolution_chain.url)
     treeQueue.await(function(error, pokeEvoChain) {
       if (error) throw error;
         pokeChain = JSON.parse(pokeEvoChain.response)
-        // console.log(pokeChain.chain)
         var pokeTree = {"name": "", "pokeID": "", "sprite": ""}
         function evoExploter(EvoChain, pokeTree){
-          // console.log(EvoChain.evolves_to)
-          // console.log(EvoChain.species.name);
           pokeTree.name = EvoChain.species.name;
-
           pokeTree.pokeID = EvoChain.species.url.substring(42).replace("/", '');
-          // console.log(pokeTree.pokeID)
           let imageQueue = d3.queue();
           imageQueue.defer(d3.request, (POKEDEX + pokeTree.pokeID))
           imageQueue.await(function(error, spritefinder) {
             if (error) throw error;
             spritefound = JSON.parse(spritefinder.response)
             pokeTree.sprite = spritefound.sprites.front_default
-            console.log(spritefound)
             pokeTree.weight = spritefound.weight
             pokeTree.speed = spritefound.stats[0].base_stat
             pokeTree.height = spritefound.height
             let typesString = ""
             for (let k = 0; k < spritefound.types.length; k++){
-               // console.log(d.types[k].type.name)
                typesString = typesString + (spritefound.types[k].type.name) + " "
               }
             pokeTree.types = typesString
             TreeFresh()
           })
-
           let chainArray = EvoChain.evolves_to;
           if (chainArray.length === 0){
             return false
@@ -68,55 +54,34 @@ function getEvolution(pokeURL){
           else{
             pokeTree.children = []
             for (let i = 0; i < chainArray.length; i++){
-              // console.log(chainArray[i].species.name)
-              // console.log(pokeTree)
               pokeTree.children.push({"name": "", "pokeID": "", "sprite": ""});
-              // pokeTree.Evolutionz[i].name = chainArray[i].species.name;
               evoExploter(chainArray[i], pokeTree.children[i]);
             }
           }
         }
         evoExploter(pokeChain.chain, pokeTree)
-
         root = d3.hierarchy(pokeTree)
-
         d3.select(".treeDrawn").remove()
         drawTree(root)
       })
     });
 }
-
 function drawTree(root){
-
   var canvas = d3.select(".treeDiagram")
     .append("g")
       .attr("class", "treeDrawn");
-
   var treeLayout = d3.tree();
   treeLayout.size([wTree, hTree - 100]);
   treeLayout(root);
-
   var nodes = root.descendants()
   var links = root.links()
 
-
-  // var node = canvas.selectAll(".node")
-  //   .data(nodes)
-  //   .enter()
-  //   .append("g")
-  //     .attr("class", "node")
-      // .attr("transform", function(d){
-      //   return ("translate("d.x + "," + d.y ")")
-      // })
-
   var diagonal = d3.linkVertical();
-
   canvas.selectAll(".link")
     .data(links)
     .enter()
     .append("line")
     .attr("class", "link")
-    // .attr("d", pathString)
     .attr("d", diagonal)
     .attr("fill", "none")
     .attr("stroke", "gray")
@@ -125,8 +90,6 @@ function drawTree(root){
     .attr('y1', function(d) {return d.source.y + 50;})
     .attr('x2', function(d) {return d.target.x;})
     .attr('y2', function(d) {return d.target.y + 50;})
-    // .curve(d3.curveLinear);
-
   canvas.selectAll("image")
     .data(nodes)
     .enter()
@@ -140,7 +103,6 @@ function drawTree(root){
     .attr("height", 100)
 
     .on("mouseover", function(d) {
-       // d3.select(this).moveToFront();
        let tooltipinfo = d.data.name + "<br/>" + "weight: " + d.data.weight
                                 + "<br/>" + "speed: " + d.data.speed
                                 + "<br/>" + "height: " + d.data.height
@@ -157,20 +119,21 @@ function drawTree(root){
          .duration(500)
          .style("opacity", 0);
        });
-
 }
 function TreeFresh(){
   canvas = d3.select(".treeDiagram")
   canvas.selectAll("image")
     .attr('xlink:href', function(d){
-      return d.data.sprite
+      if (d.data.sprite){
+        return d.data.sprite
+      }
+      else {
+        return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/79.png"
+      }
     });
-
   treeLayout = d3.tree()
   treeLayout.size([wTree, hTree - 100]);
   treeLayout(root);
-
-
   canvas.selectAll(".link")
     .transition()
     .duration(700)
@@ -178,7 +141,6 @@ function TreeFresh(){
     .attr('y1', function(d) {return d.source.y + 50;})
     .attr('x2', function(d) {return d.target.x;})
     .attr('y2', function(d) {return d.target.y + 50;})
-
   canvas.selectAll("image")
     .transition()
     .duration(700)
